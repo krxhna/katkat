eabf3f65925255e2ab335b34b428fe3f2bef335b
krxhna <m.krishnakanth1999@gmail.com>
2022-03-10 02:23:54 +0530

    ghfhgfhg

--- src/App.vue
+++ src/App.vue
@@ -4,18 +4,7 @@ import HelloWorld from '@/components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
 
   <RouterView />
 </template>
@@ -26,7 +15,7 @@ import HelloWorld from '@/components/HelloWorld.vue'
 #app {
   max-width: 1280px;
   margin: 0 auto;
-  padding: 2rem;
+ 
 
   font-weight: normal;
 }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,47 @@
 <script setup>
-import TheWelcome from '@/components/TheWelcome.vue'
+
+function kat(){
+  alert("you touched my butt");
+}
+
 </script>
 
+
+
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+<div class="hello2">
+hello
+
+
+
+<button class="btn1" @click="kat">kat's butt🍑</button>
+
+</div>
+ 
 </template>
+
+<style scoped>
+
+.hello2{
+  padding: 0;
+  margin: 0;
+  height: 100vh;
+  width: 100vw;
+  background-color: aqua ;
+
+  
+  color:red;
+  font-style: italic;
+
+}
+
+.btn1{
+  height: 20;
+  background-color: whitesmoke;
+}
+
+
+
+
+</style>
+
